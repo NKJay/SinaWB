2305de76d015633326f34af303d2d65d8b358362
Cc极生 <ccgeetion@gmail.com>
2016-08-17 14:54:48 +0800

    add qrcode scan

--- SinaWB/ViewController/Home/QRCode/QRCodeViewController.swift
+++ SinaWB/ViewController/Home/QRCode/QRCodeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class QRCodeViewController: UIViewController {
     @IBOutlet weak var scanlineView: UIImageView!
@@ -31,9 +32,9 @@ class QRCodeViewController: UIViewController {
     
     private func scanlineAnimation() {
         
-        scanlineTopConstraint.constant = -scanlineHeightConstraint.constant
-        
-        scanlineView.layoutIfNeeded()
+//        scanlineTopConstraint.constant = -scanlineHeightConstraint.constant
+//        
+//        scanlineView.layoutIfNeeded()
         
         print(scanlineTopConstraint.constant)
         UIView.animateWithDuration(2.0) {
@@ -44,11 +45,60 @@ class QRCodeViewController: UIViewController {
             self.scanlineView.layoutIfNeeded()
         }
     }
+    
+    private lazy var session:AVCaptureSession = AVCaptureSession()
+    
+    private lazy var deviceInput:AVCaptureDeviceInput? = {
+        let device = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        do {
+        let input = try AVCaptureDeviceInput(device: device)
+        return input
+        } catch {
+            print(error)
+            return nil
+        }
+    }()
+    
+    private lazy var output:AVCaptureMetadataOutput = AVCaptureMetadataOutput()
+    
+    func startScan() {
+        if !session.canAddInput(deviceInput){
+            return
+        }
+        
+        if !session.canAddOutput(output) {
+            return
+        }
+        
+        session.addOutput(output)
+        session.addInput(deviceInput)
+        
+        output.metadataObjectTypes = output.availableMetadataObjectTypes
+        
+        output.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        
+        view.layer.addSublayer(previewLayer)
+        view.layer.insertSublayer(previewLayer, atIndex: 0)
+        
+        session.startRunning()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         tabBar.selectedItem = tabBar.items![0]
+        startScan()
     }
+    
+    private lazy var previewLayer:AVCaptureVideoPreviewLayer = {
+        let layer = AVCaptureVideoPreviewLayer(session: self.session)
+        layer.frame = UIScreen.mainScreen().bounds
+        return layer
+    }()
 
+}
+
+extension QRCodeViewController:AVCaptureMetadataOutputObjectsDelegate {
+    func captureOutput(captureOutput: AVCaptureOutput!, didOutputMetadataObjects metadataObjects: [AnyObject]!, fromConnection connection: AVCaptureConnection!) {
+        print(metadataObjects.last?.stringValue)
+    }
 }
